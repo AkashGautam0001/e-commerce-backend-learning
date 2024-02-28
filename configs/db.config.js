eac311491e1ead38e5f3677b3be61bf187ee84ba
Akash Gautam <akashgautam01584@gmail.com>
2024-02-28 18:48:53 +0530

    created admin user

---
+++ configs/db.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+	DB_NAME: "ecommerce_db",
+	DB_URL: "mongodb://localhost/ecommerce_db", //0.0.0.0
+};
---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const server_config = require("./configs/server.config");
+const db_config = require("./configs/db.config");
+const user_model = require("./models/user.model");
+const bcrypt = require("bcryptjs");
+const app = express();
+
+mongoose.connect(db_config.DB_URL);
+const db = mongoose.connection;
+
+db.on("error", () => {
+	console.log("ERROR : error while connecting database");
+});
+
+db.once("open", () => {
+	console.log("MongoDB is successfully connected !");
+	init();
+});
+
+//for waiting
+async function init() {
+	let user = await user_model.findOne({
+		userId: "admin",
+	});
+	if (user) {
+		console.log("Admin is already created");
+		return;
+	}
+	try {
+		user = await user_model.create({
+			name: "Akash Gautam",
+			userId: "admin",
+			email: "akashgautam@google.com",
+			userType: "ADMIN",
+			password: bcrypt.hashSync("12345", 8),
+		});
+		console.log("Admin Created!", user);
+	} catch (err) {
+		console.log("Error while creating admin", err);
+	}
+}
+
+app.listen(server_config.PORT, () => {
+	console.log(`App started at PORT : ${server_config.PORT}`);
+});
