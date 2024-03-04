b6644c36a4a715ab4892b68f03caf0b6998d842d
Akash Gautam <akashgautam01584@gmail.com>
2024-03-04 16:52:55 +0530

    category edit,delete,fetch done

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -16,3 +16,76 @@ exports.createNewCategory = async (req, res) => {
 		});
 	}
 };
+
+exports.fatchAllCategory = async (req, res) => {
+	try {
+		const allCategories = await category_model.find({});
+		if (allCategories.length === 0)
+			res.status(204).send({
+				message: "No category found",
+			});
+
+		const requiredData = allCategories.map((category) => ({
+			category_name: category.name,
+			category_description: category.description,
+		}));
+
+		return res.status(200).send(requiredData);
+	} catch (err) {
+		return res.status(400).send({
+			message: "Error ! while fetching all category",
+		});
+	}
+};
+
+exports.editCategory = async (req, res) => {
+	const { name, description } = req.body;
+	if (!name || !description) {
+		return res.status(400).send({ message: "Name and description are required." });
+	}
+
+	try {
+		const category = await category_model.findOne({ name: req.query.name });
+		if (!category) {
+			return res.status(404).send({
+				message: "category not found",
+			});
+		}
+
+		await category_model.findOneAndUpdate(
+			{ _id: category._id },
+			{
+				$set: { name: name, description: description },
+			}
+		);
+
+		return res.status(200).send({
+			message: `Successfully edited !`,
+		});
+	} catch (err) {
+		console.log("Errror while editing category", err);
+		res.status(500).send({
+			message: "Error while editing category",
+		});
+	}
+};
+
+exports.deleteCategory = async (req, res) => {
+	try {
+		const category = await category_model.findOne({ name: req.body.name });
+		if (!category) {
+			return res.status(400).send({
+				message: "category not found !",
+			});
+		}
+		await category_model.deleteOne(category);
+		return res.status(200).send({
+			message: `Successfully deleted !`,
+		});
+	} catch (err) {
+		console.log("Errror while deleting category", err);
+		res.status(500).send({
+			message: "Error while deleting category",
+		});
+	}
+};
--- routes/category.route.js
+++ routes/category.route.js
@@ -2,4 +2,10 @@ const category_controller = require("../controllers/category.controller");
 const auth_mw = require("../middlewares/auth.mw");
 module.exports = (app) => {
 	app.post("/ecomm/api/v1/auth/category", [auth_mw.verifyToken, auth_mw.isAdmin], category_controller.createNewCategory);
+
+	app.delete("/ecomm/api/v1/auth/category", [auth_mw.verifyToken, auth_mw.isAdmin], category_controller.deleteCategory);
+
+	app.patch("/ecomm/api/v1/auth/category", [auth_mw.verifyToken, auth_mw.isAdmin], category_controller.editCategory);
+
+	app.get("/ecomm/api/v1/auth/category", category_controller.fatchAllCategory);
 };
